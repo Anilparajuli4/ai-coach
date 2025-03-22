62a356be169dfaef55b971baff9a608089c8355b
Anil Parajuli <anilparajuli998@gmail.com>
2025-03-22 08:39:50 +0545

    changes in onboarded

--- online-coach/app/(main)/onboarding/page.jsx
+++ online-coach/app/(main)/onboarding/page.jsx
@@ -1,10 +1,11 @@
+export const dynamic = 'force-dynamic'; //Add this line
 import { industries } from '@/data/industries'
 import React from 'react'
 import OnboardingForm from './_components/OnboardingForm'
 import { getUserOnboardingStatus, updateUser } from '@/actions/User'
 import { redirect } from 'next/navigation'
 
-export const dynamic = 'force-dynamic'; // Add this line
+
 
 
 async function Onboarding() {
